f3671d50ac3f62035bd8ed2ea8f4143f57be853d
Jin <18898791+drliangjin@users.noreply.github.com>
2018-04-23 21:49:12 +0100

    Update caffeine.lua

--- modules/caffeine.lua
+++ modules/caffeine.lua
@@ -1,1 +1,21 @@
-
+-- ------------------------------------------------------------------
+-- Caffeine Replacement - Keep display awake when caffeine is active.
+-- ------------------------------------------------------------------
+local caffeine = hs.menubar.new()
+ 
+function setCaffeineDisplay(state)
+    if state then
+        caffeine:setIcon(os.getenv("HOME") .. "/.hammerspoon/assets/caffeine/active.png")
+    else
+        caffeine:setIcon(os.getenv("HOME") .. "/.hammerspoon/assets/caffeine/inactive.png")
+    end
+end
+ 
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+ 
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
