badf84585048e6ed7fcb14fa3e37caa1f4d97d8b
Liang Jin <18898791+drliangjin@users.noreply.github.com>
2018-06-02 01:28:37 +0100

    Update and rename launcher.lua to toggleApplication.lua

---
+++ modules/toggleApplication.lua
@@ -0,0 +1,16 @@
+-- Launch/Hide applications
+
+-- hs.application.enableSpotlightForNameSearches(false)
+
+-- TODO: the following cannot deal with minimised applications
+
+function toggleApplication(name)
+    local app = hs.application.find(name)
+    if app and app:isFrontmost() then
+        app:hide()
+    else
+        hs.application.launchOrFocus(name) -- twice to unminimise app
+    end
+end
+
+hs.hotkey.bind(hyper, "return", function() toggleApplication("iTerm") end)
