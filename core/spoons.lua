6be654482511aa174a2f54ebeffa933e6d6293eb
Liang Jin <18898791+drliangjin@users.noreply.github.com>
2018-06-05 11:53:48 +0100

    Update spoons.lua

--- core/spoons.lua
+++ core/spoons.lua
@@ -1,3 +1,11 @@
 -- spoons.lua - Spoons Installation and Management
 
 hs.loadSpoon("SpoonInstall")
+
+spoon.SpoonInstall.use_syncinstall = true
+
+spoon.SpoonInstall:andUse("KSheet",
+               {
+                 hotkeys = {
+                   toggle = { hyper, "k" }
+}})
