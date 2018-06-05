ddd4b725980138d724ffd60b039415ff954b07c8
Liang Jin <18898791+drliangjin@users.noreply.github.com>
2018-06-05 23:15:19 +0100

    Update and rename spoons.lua to spoon-install.lua

---
+++ core/spoon-install.lua
@@ -0,0 +1,14 @@
+-- spoons.lua - Spoons Installation and Management
+
+hs.loadSpoon("SpoonInstall")
+
+-- 
+spoon.SpoonInstall.repos = {
+  default = {
+    url = "https://github.com/Hammerspoon/Spoons",
+    desc = "Main Hammerspoon Spoon Repository",
+  }
+}
+
+-- :andUse() will update repos and install packages synchronously
+spoon.SpoonInstall.use_syncinstall = true
