1bc4ea3d6be7018787967d9a4e1c03898bbbbded
Liang Jin <18898791+drliangjin@users.noreply.github.com>
2018-06-09 22:58:34 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -36,15 +36,41 @@ hs.hints.showTitleThresh = 0
 hs.window.animationDuration = 0
 
 -------------------------------
--- Spoons
+-- SpoonInstall
 -------------------------------
--- Core functionality
-require("core/spoon-install")
+-- "SpoonInstall" is downloaded using the tool/install script
+hs.loadSpoon("SpoonInstall")
+
+-- Add official github Spoons repos
+spoon.SpoonInstall.repos = {
+  default = {
+    url = "https://github.com/Hammerspoon/Spoons",
+    desc = "Main Hammerspoon Spoon Repository",
+  }
+}
+
+-- Update default Spoons repos
+spoon.SpoonInstall:updateRepo()
+
+-- Download and install spoons
+spoon.SpoonInstall:installSpoonFromRepo("Caffeine")
+
+-------------------------------
+-- Hotkey Modal
+-------------------------------
+hsMode = hs.hotkey.modal.new(hyper, 'h')
+
+function hsMode:entered()
+  hs.alert'Entered Hammerspoon mode' 
+end
+
+function hsMode:exited()
+  hs.alert'Exited Hammerspoon mode'
+end
 
 -------------------------------
 -- Caffeine
 -------------------------------
-spoon.SpoonInstall:installSpoonFromRepo("Caffeine")
 hs.loadSpoon("Caffeine")
 spoon.Caffeine:bindHotkeys({toggle={hyper, "1"}})
 spoon.Caffeine:start()
