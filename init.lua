bca3767b73f7609c3521fae4c288139e1474b76e
Liang Jin <18898791+drliangjin@users.noreply.github.com>
2018-06-09 20:23:19 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,10 +4,46 @@
 -- See Karabiner-Elements configuration: hyper is mapped to left control
 hyper = { 'shift', 'ctrl', 'alt', 'cmd' }
 
+-------------------------------
+-- General Preferences
+-------------------------------
+-- Launch Hammerspoon when login
+hs.autoLaunch(true)
+
+-- Check for Hammerspoon updates
+hs.automaticallyCheckForUpdates(false)
+
+-- Show Hammerspoon menu icon
+hs.menuIcon(true)
+
+-- Hide Hammerspoon dock icon
+hs.dockIcon(false)
+
+-- Keep Console window on top
+hs.consoleOnTop([state])
+
+-- Send crash data to Hammerspoon
+hs.uploadCrashData(true)
+
+-------------------------------
+-- Advanced Settings
+-------------------------------
+-- 
+hs.hotkey.alertDuration = 0
+--
+hs.hints.showTitleThresh = 0
+--
+hs.window.animationDuration = 0
+
+-------------------------------
+-- Spoons
+-------------------------------
 -- Core functionality
 require("core/spoon-install")
 
--- Modules
-require("modules/windows")
-require("modules/toggle-application")
-require("modules/switch-browser")
+-------------------------------
+-- Caffeine
+-------------------------------
+hs.loadSpoon("Caffeine")
+spoon.Caffeine:bindHotkeys({toggle={hyper, "1"}})
+spoon.Caffeine:start()
