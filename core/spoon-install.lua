b15d90198ca794c07b7c53ad2e8977c209dc54d1
Liang Jin <18898791+drliangjin@users.noreply.github.com>
2018-06-09 19:28:42 +0100

    Update spoon-install.lua

--- core/spoon-install.lua
+++ core/spoon-install.lua
@@ -1,7 +1,10 @@
--- spoons.lua - Spoons Installation and Management
+-- spoon-install.lua - Spoons Manager 
 
+-- "SpoonInstall" is downloaded using the tool/install script
+-- then other Spoons can be downloaded and loaded using "SpoonInstall"
 hs.loadSpoon("SpoonInstall")
 
+-- add official github Spoons repos
 spoon.SpoonInstall.repos = {
   default = {
     url = "https://github.com/Hammerspoon/Spoons",
@@ -11,3 +14,5 @@ spoon.SpoonInstall.repos = {
 
 -- :andUse() will update repos and install packages synchronously
 spoon.SpoonInstall.use_syncinstall = true
+
+-- spoon-install.lua ends here
